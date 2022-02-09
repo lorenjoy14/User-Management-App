9f979dc34db11cc68130fe128166d3424b8bac9f
lorenjoy14 <99311479+lorenjoy14@users.noreply.github.com>
2022-02-09 18:32:35 +0800

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,28 @@
+<div class="well card-footer">
+  <p>Website: https://user-management-app.com/
+      <span class="float-right">Like us: https://www.facebook.com/Lj Sabas/</span>
+  </p>
+</div>
+
+
+
+
+  </body>
+
+
+
+
+  <!-- Jquery script -->
+  <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap.min.js"></script>
+  <script src="assets/jquery.dataTables.min.js"></script>
+  <script src="assets/dataTables.bootstrap4.min.js"></script>
+  <script>
+      $(document).ready(function () {
+          $("#flash-msg").delay(7000).fadeOut("slow");
+      });
+      $(document).ready(function() {
+          $('#example').DataTable();
+      } );
+  </script>
+</html>
